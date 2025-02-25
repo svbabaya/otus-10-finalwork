7aeaf56f7987c945c8fbc87c3b3e9715495c0b55
Serge Babayan <babayan.serge@gmail.com>
2025-02-25 17:29:45 +0300

    Remove cpr and indicators

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,6 @@
 #include <fmt/core.h>
 #include <fmt/color.h>
 #include <CLI/CLI.hpp>
-#include <cpr/cpr.h>
-
-#include <indicators/progress_bar.hpp>
-#include <thread>
-#include <chrono>
 
 int main(int argc, char** argv) {
 
@@ -22,35 +17,6 @@ int main(int argc, char** argv) {
 
     std::string s = "Abc\n";
     fmt::print(fg(fmt::color::red), s); 
-
-    using namespace indicators;
-    ProgressBar bar{
-        option::BarWidth{50},
-        option::Start{"["},
-        option::Fill{"="},
-        option::Lead{">"},
-        option::Remainder{" "},
-        option::End{"]"},
-        option::PostfixText{"Extracting Archive"},
-        option::ForegroundColor{Color::green},
-        option::FontStyles{std::vector<FontStyle>{FontStyle::bold}}
-    };
-    while (true) {
-        bar.tick();
-        if (bar.is_completed())
-            break;
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
-
-
-    cpr::Response r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
-        cpr::Authentication{"user", "pass", cpr::AuthMode::BASIC},
-        cpr::Parameters{{"anon", "true"}, {"key", "value"}});
-    r.status_code;                  // 200
-    r.header["content-type"];       // application/json; charset=utf-8
-    // r.text;                         // JSON text string
-
-    fmt::print("The answer is {}\n", r.text);
 
 
     // Add new options/flags here
